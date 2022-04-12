8afc49f58ae827a1e1c02e99e4477c87bb4dc615
Ryan Spears <spearswesley5@gmail.com>
2022-04-12 14:14:44 -0500

    Created Number-Gen Game

--- num-gen/src/App.js
+++ num-gen/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Numble</h1>
       </header>
+      <section className='mainSec'>
+        <h3>Try to guess the correct number.</h3>
+        <h3>You have 5 guesses.</h3>
+        <h3>The correct number will be anywhere from 0 to 100.</h3>
+        <h3>Good Luck!</h3>
+      </section>
+      <section className="gameArea">
+        <input id="numGuess" className="numSelect" type="number" />
+        <h4>Tries Left: </h4>
+        <h3 id="triesLeft">5</h3>
+        <button className="numEnter" onClick={() => guess(document.getElementById('numGuess').value)}>GUESS</button>
+        <div id="guessList"></div>
+      </section>
     </div>
   );
+}
+
+function numGen() {
+  var chance = require('chance');
+  var rand = new chance();
+
+  var value = rand.integer({ min: 0, max: 100 });
+
+  return value;
+}
+
+const random = numGen();
+
+console.log(random);
+
+var guessesLeft = 5;
+
+function guess(num) {
+  if(num == random) {
+    document.getElementById("guessList").innerHTML += '<h3>' + num + ', </h3>';
+    document.getElementById("guessList").innerHTML = '<h1>That is correct! Refresh the page to play again.</h1>';
+    return;
+  }
+  else if(num != random && guessesLeft != 0) {
+    guessesLeft--;
+    document.getElementById("triesLeft").innerHTML = guessesLeft;
+    document.getElementById("guessList").innerHTML += '<h3>' + num + ', </h3>';
+  }
+  else {
+    document.getElementById("guessList").innerHTML = '<h1>You ran out of guesses! Refresh the page to try again.</h1>';
+    return;
+  }
 }
 
 export default App;
